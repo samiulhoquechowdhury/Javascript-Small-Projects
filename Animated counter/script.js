3d4d4e29565ce52afb3c4291cc401b62628865fd
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-03 00:20:08 +0530

    added js file

--- Animated counter/script.js
+++ Animated counter/script.js
@@ -1,1 +1,26 @@
-const counter = document.querySelectorAll(".counter");
+
+const counters = document.querySelectorAll('.counter');
+const speed = 1000;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+
+        const animation = Math.ceil(target / speed);
+
+
+        if (count < target) {
+
+            counter.innerText = count + animation;
+
+            setTimeout(updateCount, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
+
