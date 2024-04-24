0b2919ee710fc52c9ae13508d7a8354149bff216
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-24 23:50:18 +0530

    Captcha Generator

--- Capta Generator/script.js
+++ Capta Generator/script.js
@@ -1,3 +1,34 @@
 let captcha;
 let alphabets = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
 let status = document.getElementById('status');
+// status.innerText = "Captcha Generator";
+
+function generate(){
+    let first = alphabets[Math.floor(Math.random() * alphabets.length)]
+    let second = Math.floor(Math.random() * 10)
+    let Third = Math.floor(Math.random() * 10)
+    let forth = alphabets[Math.floor(Math.random() * alphabets.length)]
+    let fifth = alphabets[Math.floor(Math.random() * alphabets.length)]
+    let sixth = Math.floor(Math.random() * 10)
+
+    captcha = first.toString() + second.toString() + Third.toString() + forth.toString() + fifth.toString() + sixth.toString()
+
+    document.getElementById('generated-captcha').value = captcha;
+    document.getElementById('entered-captcha').value = '';
+    // status.innerText = "Captcha Generator"//
+}
+
+function check(){
+    let userValue = document.getElementById('entered-captcha').value;
+
+    if (userValue == captcha) {
+        status.innerText = "Correct!"
+    } else {
+        status.innerText = "Try Again!!"
+        document.getElementById('entered-captcha')
+
+    }
+}
+
+
