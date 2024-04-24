d4d46fa0b163a8f072bba15fff700b092126c01b
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-04-25 00:20:13 +0530

    Added some new features

--- Capta Generator/script.js
+++ Capta Generator/script.js
@@ -2,32 +2,43 @@ let captcha;
 let alphabets = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
 let status = document.getElementById('status');
-// status.innerText = "Captcha Generator";
+let previousColor; // Store previous color
+
+status.innerText = "Captcha Generator";
 
 function generate(){
     let first = alphabets[Math.floor(Math.random() * alphabets.length)]
     let second = Math.floor(Math.random() * 10)
-    let Third = Math.floor(Math.random() * 10)
+    let third = Math.floor(Math.random() * 10)
     let forth = alphabets[Math.floor(Math.random() * alphabets.length)]
     let fifth = alphabets[Math.floor(Math.random() * alphabets.length)]
     let sixth = Math.floor(Math.random() * 10)
 
-    captcha = first.toString() + second.toString() + Third.toString() + forth.toString() + fifth.toString() + sixth.toString()
+    captcha = first.toString() + second.toString() + third.toString() + forth.toString() + fifth.toString() + sixth.toString()
 
     document.getElementById('generated-captcha').value = captcha;
     document.getElementById('entered-captcha').value = '';
-    // status.innerText = "Captcha Generator"//
+    status.innerText = "Captcha Generator";
+    status.style.color = previousColor; // Restore previous color
 }
 
-function check(){
+function check() {
     let userValue = document.getElementById('entered-captcha').value;
 
-    if (userValue == captcha) {
-        status.innerText = "Correct!"
+    if (userValue === captcha) {
+        status.innerText = "Correct!";
+        status.style.color = 'green';
     } else {
-        status.innerText = "Try Again!!"
-        document.getElementById('entered-captcha').value = '';
+        previousColor = status.style.color;
+        status.innerText = "Try Again!!";
+        status.style.color = 'red';
+        setTimeout(generate, 2000); 
     }
 }
 
-
+const entered = document.getElementById('entered-captcha');
+entered.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        check();
+    }
+});
